7f32706db20763c13f254665570d5240c1022bcc
Reema <rashadid@mail.csuchico.edu>
2022-02-20 19:13:49 -0800

    working super-MVP

--- CountryMap.h
+++ CountryMap.h
@@ -11,7 +11,7 @@ class CountryMap {
     private:
         map<Country, unsigned> Map;
         unsigned MAX_DAYS = 90;
-        unsigned usedDays;
+        //unsigned usedDays;
         void     populateMap( map<Country,unsigned>, string fileName );
     
     public:
@@ -19,7 +19,9 @@ class CountryMap {
         bool     inSchengen(string name) const;
         unsigned getUsedDays() const;
         void     setUsedDays(unsigned newDayCount);
+        void     setCountryDays(string name, unsigned days);
         unsigned getCountryDays(string name) const;
+        unsigned getMaxDays() const;
         void     showMap();
 
 
@@ -28,7 +30,7 @@ class CountryMap {
 
 // Default Constructor
     CountryMap::CountryMap() {
-        usedDays = 0;
+        //usedDays = 0;
         populateMap(Map, "SchengenCountries.txt");
     }
 
@@ -40,49 +42,39 @@ class CountryMap {
         string   countryName;
 
         fileStream.open(fileName);
-        if (!fileStream.is_open()) {               // output error message if open fails
+        if (!fileStream.is_open()) {                 // output error message if open fails
             cout << "Could not open " << fileName;
         }
         
         getline( fileStream, countryName );          // read first line of country file
         
         while (!fileStream.fail()) {
-            //cout << "inserting " << countryName << endl;
             Country country(countryName);
-            Map.insert( make_pair(country,0) );     // insert each country into map with 0 days
+            Map.insert( make_pair(country,0) );      // insert each country into map with 0 days
             getline( fileStream, countryName );      // get next line
         }
         cout << "map size: " << Map.size() << endl;
-        fileStream.close();                        // close file
+        fileStream.close();                          // close file
     }
+
 
 // List Countries and Days in order of days
     void CountryMap::showMap() {
         
-        cout << "Current Country List:" << endl;
+        //cout << "Current Country List:" << endl;
 
-        for (auto it= Map.begin(); it != Map.end(); it++) {
-            
-            // cout.flush();
-            if (it->first.getName() == "Switzerland") {
-                it->second = 2;
-            }
-            
-            // cout << it->first.getName();
-            // cout << "name size: " << (it->first.getName()).length() << endl;
-            // cout << "Days: " << it->second << endl;
-
-            cout << it->first.getName();
-            cout.flush();
-            cout << ":" << it->second << endl;
+        for (auto it= Map.begin(); it != Map.end(); it++) {   
+            cout << it->first.getName() << ": " << it->second << endl;
         }
+
+        cout << endl << "Total days used: " << getUsedDays() << "/" << MAX_DAYS << endl << endl;
     }
 
 
 // Check if country inquiry is within the Schengen region
     bool CountryMap::inSchengen(string name) const {
         
-        if (Map.count(name) > 0) {
+        if (Map.count(name) > 0) {  // or find?
             return true;
         } else {
             return false;
@@ -91,17 +83,31 @@ class CountryMap {
 
 // How many days have been used
     unsigned CountryMap::getUsedDays() const {
+        
+        unsigned usedDays= 0;
+        for (auto it= Map.begin(); it != Map.end(); it++) {   
+            usedDays += it->second;
+        }
         return usedDays;
     }
 
 // Fetch Country object from map
     unsigned CountryMap::getCountryDays(string name) const {
         
-        //unsigned days = it->getDays();
         return Map.find(name)->second;
     }
 
-// Update used day count
-    void CountryMap::setUsedDays(unsigned newDayCount) {
-        usedDays = newDayCount;
+// // Update used day count
+//     void CountryMap::setUsedDays(unsigned newDayCount) {
+//         usedDays = newDayCount;
+//     }
+
+// Set days in Country
+    void CountryMap::setCountryDays(string name, unsigned days) {
+        Map.find(name)->second = days;
+    }
+
+// Get MAX_DAYS value
+    unsigned CountryMap::getMaxDays() const {
+        return MAX_DAYS;
     }
--- Schuffle.cpp
+++ Schuffle.cpp
@@ -7,12 +7,35 @@ using namespace std;
 
 int main() {
 
+    string countryName;
+    unsigned days;
+    CountryMap myMap;
     
     cout << "Welcome to Schengen Shuffle!" << endl;
-
-    CountryMap myMap;
+    cout << "Here is a list of all the countries within the Schengen Region." << endl;
+    cout << "You have " << myMap.getMaxDays() << " to move between these countries:" << endl;
 
     myMap.showMap();
+
+    cout << "Which country days would you like to edit? (enter 'quit' to exit)" << endl;
+    cin >> countryName;
+
+    while ( countryName != "quit" ) {
+
+        if (myMap.inSchengen(countryName)) {
+            cout << "How many days?" << endl;
+            cin >> days;
+            myMap.setCountryDays(countryName, days);
+
+        } else {     
+            cout << endl << "Sorry, " << countryName << " is not in the Schengen region or is not recognized." << endl;
+            cout << "Here is your current list:" << endl;
+        }
+        cout << "Your current list:" << endl;
+        myMap.showMap();
+        cout << "Which country days would you like to edit? (enter 'quit' to exit)" << endl;
+        cin >> countryName;
+    }
 
 
     return 0;
